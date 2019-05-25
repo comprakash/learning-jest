6cdef8a5f942426188fd9d6ad9a5249fa1411229
Om Prakash C <comprakash@gmail.com>
2019-05-25 15:07:06 +0530

    Added store setup and updated index.js

--- jest-with-redux/src/index.js
+++ jest-with-redux/src/index.js
@@ -1,12 +1,13 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import reducers from './reducers/index';
+import DogApp from './components/dog-app/dog-app';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(reducers, applyMiddleware(thunk));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<Provider store={store}><DogApp /></Provider>, document.getElementById('root'));
