0fcddf99b5f25ca5231673de1824a938dc8755ca
Om Prakash C <comprakash@gmail.com>
2019-05-25 14:36:20 +0530

    Dog Reducer

---
+++ jest-with-redux/src/reducers/dog/dog-reducer.js
@@ -0,0 +1,19 @@
+
+import { FETCH_DOG_SUCCESS } from '../../constants/action-types';
+
+export const initialState = {
+  url: '',
+};
+
+export default function dogReducer(state = initialState, action) {
+  switch (action.type) {
+    case FETCH_DOG_SUCCESS: {
+      return {
+        ...state,
+        url: action.payload.url,
+      }
+    }
+    default:
+      return state;
+  }
+}
